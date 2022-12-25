a153d2d30dc06134d28394836f746d427204f876
papaibrahima12 <papaibrahimasow@esp.sn>
2022-12-25 18:14:14 +0000

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
-    './node_modules/tw-elements/dist/js/**/*.js'
+    './node_modules/tw-elements/dist/js/**/*.js',
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
@@ -11,7 +13,12 @@ module.exports = {
       }
     },
   },
+  screens: {
+    xs: '475px',
+    ...defaultTheme.screens,
+  },
   plugins: [
-    require('tw-elements/dist/plugin')
+    require('tw-elements/dist/plugin'),
+    require('flowbite/plugin'),
   ],
 }
